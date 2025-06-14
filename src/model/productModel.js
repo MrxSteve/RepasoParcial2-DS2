b95e331c63f15dea8342c73481cdac1970af529c
mrxSteve <steventobar38@gmail.com>
2025-06-14 01:32:18 -0600

    feat: producto model

---
+++ src/model/productModel.js
@@ -0,0 +1,35 @@
+import { DataTypes } from 'sequelize';
+
+export const productModel = async (sequelize) => {
+    const producto = sequelize.define('products', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        nombre: {
+            type: DataTypes.STRING(100),
+            allowNull: false, 
+        },
+        marca: {
+            type: DataTypes.STRING(100),
+            allowNull: true,
+        },
+        categoria: {
+            type: DataTypes.STRING(100),
+            allowNull: true,
+            unique: true,
+        },
+        picture: {
+            type: DataTypes.STRING(255),
+            allowNull: true,
+        },
+        precio: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: true,
+        }
+    }, {
+        timestamps: false,
+    });
+    return producto;
+}
